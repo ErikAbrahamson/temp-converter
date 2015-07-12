36597c5bb467f5b78f17930edc86a669f6282220
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-07-12 12:14:09 -0600

    Solution for temp converter

---
+++ tempconverter.js
@@ -0,0 +1,14 @@
+//My solution for the temperature converter
+var initialPrompt = prompt('Would you like to convert from Celcius or Fahrenheit first?', 'please type either celcius or fahrenheit');
+var input = parseInt(prompt('Please enter a number.'));
+var validInput = initialPrompt.toLowerCase();
+
+if (validInput === 'celcius') {
+  var newNum = Math.ceil(input * 1.8 + 32);
+  alert('Your number in Fahrenheit is ' + newNum);
+} else if (validInput === 'fahrenheit') {
+  var newNum = Math.ceil((input - 32) / 1.8);
+  alert('Your number in Fahrenheit is ' + newNum);
+} else if (validInput != ('fahrenheit' || 'celcius')){
+  alert('You did not enter a temperature type!');
+} 
